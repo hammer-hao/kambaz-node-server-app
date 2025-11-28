faa3a855aaccbf6a2cb0529a0277018ffbcd2bf3
hammer-hao <zihehaomichael@gmail.com>
2025-11-27 19:12:52 -0500

    a6

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,29 +1,35 @@
+import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
-export default function UsersDao(db) {
-    let { users } = db;
-    const findAllUsers = () => users;
-    const findUserById = (userId) => users.find((user) => user._id === userId);
-    const findUserByCredentials = (username, password) =>
-        users.find((user) => user.username === username && user.password === password);
-    const updateUser = (userId, user) => (users = users.map((u) => (u._id === userId ? user : u)));
-    const deleteUser = (userId) => (users = users.filter((u) => u._id !== userId));
+export default function UsersDao() {
     const createUser = (user) => {
         const newUser = { ...user, _id: uuidv4() };
-        users = [...users, newUser];
-        return newUser;
-    };
-    const findUserByUsername = (username) => users.find((user) => user.username === username);
-
-    function findUsersForCourse(courseId) {
-        const { users, enrollments } = db;
-        return users.filter((user) =>
-            enrollments.some(
-                (enrollment) =>
-                    enrollment.user === user._id && enrollment.course === courseId
-            )
-        );
+        return model.create(newUser);
     }
+    const findAllUsers = () => model.find();
+    const findUsersByRole = (role) => model.find({ role: role });
+    const findUserById = (userId) => model.findById(userId);
+    const findUserByUsername = (username) =>  model.findOne({ username: username });
+    const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
+    const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
+    const deleteUser = (userId) => model.findByIdAndDelete( userId );
+    const findUsersByPartialName = (partialName) => {
+        const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+        return model.find({
+            $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+        });
+    };
+
+
+    // function findUsersForCourse(courseId) {
+    //     const { users, enrollments } = db;
+    //     return users.filter((user) =>
+    //         enrollments.some(
+    //             (enrollment) =>
+    //                 enrollment.user === user._id && enrollment.course === courseId
+    //         )
+    //     );
+    // }
 
     return {
-        createUser, findAllUsers, findUserById, findUserByUsername, findUserByCredentials, updateUser, deleteUser, findUsersForCourse };
+        createUser, findAllUsers, findUserById, findUserByUsername, findUserByCredentials, updateUser, deleteUser, findUsersByRole, findUsersByPartialName };
 }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from "mongoose"
 import Hello from "./hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -10,6 +11,9 @@ import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
 import "dotenv/config";
 import session from "express-session";
+
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+await mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(
